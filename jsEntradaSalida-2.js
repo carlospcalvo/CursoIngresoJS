29e44cc66e7f65256a19f10276d50dfcf22d8663
carlospcalvo <50934366+carlospcalvo@users.noreply.github.com>
2019-07-04 11:23:46 -0300

    Clase 1 - Int. a la Programación

---
+++ jsEntradaSalida-2.js
@@ -0,0 +1,18 @@
+/*Debemos lograr tomar un nombre con 'prompt'
+y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
+
+/*Enunciado:
+Al presionar el botón 'mostrar'
+debemos lograr tomar un dato por 'PROMPT'
+y luego mostrarlo por 'ALERT'.*/
+
+function mostrar()
+{
+  var nombre;
+
+  nombre = prompt("Ingrese su nombre")
+
+  alert(nombre)
+
+}
+
