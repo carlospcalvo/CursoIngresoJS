1e81255a9424f486bd9d955ef65155c7f3411b9c
Carlos Calvo Nazábal <50934366+carlospcalvo@users.noreply.github.com>
2019-07-18 12:16:03 -0300

    18/7

--- 3-InstruccionSwitch/jsInstruccionSwitch(2).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(2).js
@@ -1,9 +1,78 @@
+/*
+al seleccionar un mes informar.
+si estamos en Invierno: "Abrigate que hace frio."
+si aún no llego el Invierno: "Falta para el invierno."
+si ya paso el Invierno: "Ya pasamos el frio, ahora calor!!!."
+ACLARAcIÓN: tomamos a Julio y Agosto como los meses de Invierno.
+*/
+
 function mostrar()
 {
-//tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
+  var mesDelAño = document.getElementById('mes').value;
 
-alert (mesDelAño);
+  switch(mesDelAño)
+  {
+    case "Enero":
+    {
+      alert("Winter is coming.");
+      break;
+    }
+    case "Febrero" :
+    {
+      alert("Winter is coming.");
+      break;
+    }
+    case "Marzo":
+    {
+      alert("Winter is coming.");
+      break;
+    }
+    case "Abril":
+    {
+      alert("Winter is coming.");
+      break;
+    }
+    case "Mayo":
+    {
+      alert("Winter is coming.");
+      break;
+    }
+    case "Junio":
+    {
+      alert("Winter is coming.");
+      break;
+    }
+    case "Julio":
+    {
+      alert("Winter is here.");
+      break;
+    }
+    case "Agosto":
+    {
+      alert("Winter is here.");
+      break;
+    }
+    case "Septiembre":
+    {
+      alert("Winter is gone.");
+      break;
+    }
+    case "Octubre":
+    {
+      alert("Winter is gone.");
+      break;
+    }
+    case "Noviembre":
+    {
+      alert("Winter is gone.");
+      break;
+    }
+    case "Diciembre":
+    {
+      alert("Winter is gone.");
+      break;
+    }
+  }
 
 
 
--- 3-InstruccionSwitch/jsInstruccionSwitch(3).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(3).js
@@ -1,11 +1,25 @@
+/*
+al seleccionar un mes informar.
+si es Febrero: " Este mes no tiene más de 29 días."
+si NO es Febrero: "Este mes tiene 30 o más días"
+*/
+
 function mostrar()
 {
-//tomo la edad  
+//tomo la edad
 var mesDelAño = document.getElementById('mes').value;
 
-//alert (mesDelAño);
-	
-	
-
+switch(mesDelAño)
+{
+  case "Febrero" :
+  {
+    alert("Este mes no tiene mas de 29 dias");
+    break;
+  }
+  default:
+  {
+    alert("Este mes tiene 30 o más días");
+  }
+}
 
 }//FIN DE LA FUNCIÓN
